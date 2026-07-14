5dca6ce31cfa24c204706d6001029c52446f9b26
kkiyong123 <98229313+kkiyong123@users.noreply.github.com>
2026-07-14 22:30:10 +0900

    Add src/main/java/lk/gov/ird/etax/report/EtaxReportApplication.java

---
+++ src/main/java/lk/gov/ird/etax/report/EtaxReportApplication.java
@@ -0,0 +1,11 @@
+package lk.gov.ird.etax.report;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EtaxReportApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(EtaxReportApplication.class, args);
+    }
+}
